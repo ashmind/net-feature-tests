1b4cbdbd14514b4e5f09880c3b77086a1ff55b91
ashmind <ashmind@9071a3dc-7de4-11dd-98da-f1285d4915c4>
2008-09-12 15:51:50 +0000

    Updated StructureMap adapter according to http://codebetter.com/blogs/jeremy.miller/archive/2008/09/10/using-the-structuremap-container-independently-of-objectfactory.aspx , no more static classes. Post will be soon updated as well.

--- AshMind.Research.IoC.Frameworks.Tests/Adapters/StructureMapAdapter.cs
+++ AshMind.Research.IoC.Frameworks.Tests/Adapters/StructureMapAdapter.cs
@@ -5,55 +5,66 @@ using System.Text;
 
 using StructureMap;
 using StructureMap.Configuration.DSL;
+using StructureMap.Pipeline;
 
 namespace AshMind.Research.IoC.Frameworks.Tests.Adapters {
     public class StructureMapAdapter : IFrameworkAdapter {
-        public StructureMapAdapter() {
-            // In general, I like StructureMap API, but having to choose from
-            //   ObjectFactory.ReInitialize();
-            //   ObjectFactory.Reset();
-            //   ObjectFactory.ResetDefaults();
-            //   StructureMapConfiguration.ResetAll();
-            // is a bit too much (each of these actualy does a different thing)
+        private readonly Registry registry;
+        private Container container;
 
-            StructureMapConfiguration.ResetAll();
-            ObjectFactory.ReInitialize(); 
+        public StructureMapAdapter() {
+            // wow we do not have to use statics
+            registry = new Registry();
         }
 
         public void RegisterSingleton<TComponent, TService>()
             where TComponent : TService
         {
-            StructureMapConfiguration.ForRequestedType<TService>()
-                                     .TheDefaultIsConcreteType<TComponent>()
-                                     .AsSingletons();
+            registry.ForRequestedType<TService>()
+                    .TheDefaultIsConcreteType<TComponent>()
+                    .AsSingletons();
         }
 
         public void RegisterTransient<TComponent, TService>()
             where TComponent : TService
         {
-            StructureMapConfiguration.ForRequestedType<TService>()
-                                     .TheDefaultIsConcreteType<TComponent>();
+            registry.ForRequestedType<TService>()
+                    .TheDefaultIsConcreteType<TComponent>();
         }
 
         public void Register(Type componentType, Type serviceType) {
-            StructureMapConfiguration.ForRequestedType(serviceType)
-                                     .TheDefaultIsConcreteType(componentType);
+            registry.ForRequestedType(serviceType)
+                    .TheDefaultIsConcreteType(componentType);
         }
 
         public void Register<TService>(TService instance) {
-            StructureMapConfiguration.ForRequestedType<TService>()
-                                     .TheDefaultIs(
-                                          Registry.Object<TService>(instance)
-                                     );
+            registry.ForRequestedType<TService>()
+                    .TheDefaultIs(
+                        Registry.Object(instance)
+                    );
+        }
+
+        private void EnsureContainer() {
+            if (container != null)
+                return;
+
+            container = new Container(this.registry);
         }
 
         public TService Resolve<TService>() {
-            return ObjectFactory.GetInstance<TService>();
+            this.EnsureContainer();
+            return container.GetInstance<TService>();
         }
 
         public TService Resolve<TService>(IDictionary<string, object> additionalArguments) {
-            // haven't figured out a way to do this
-            throw new NotSupportedException();
+            this.EnsureContainer();
+            var additionalArgumentsAsDictionary = additionalArguments.ToDictionary(
+                arg => arg.Key, arg => arg.Value
+            ); // a pity StructureMap wants a concrete type
+
+            return container.GetInstance<TService>(
+                new ExplicitArguments(additionalArgumentsAsDictionary)
+            );
         }
 
         public TComponent Create<TComponent>() {
