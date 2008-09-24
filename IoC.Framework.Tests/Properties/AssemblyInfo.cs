508ba2248c427cf6104323a5b918e94e97d4e255
ashmind <ashmind@9071a3dc-7de4-11dd-98da-f1285d4915c4>
2008-09-24 07:55:58 +0000

    Renamed the projects to be less author-specific.

--- IoC.Framework.Tests/Properties/AssemblyInfo.cs
+++ IoC.Framework.Tests/Properties/AssemblyInfo.cs
@@ -1,18 +1,11 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AshMind.Research.IoC.Frameworks.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AshMind.Research.IoC.Frameworks.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2008")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("IoC.Framework.Tests")]
+[assembly: AssemblyProduct("IoC.Framework.Tests")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
