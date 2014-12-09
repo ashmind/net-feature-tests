344ed9f2b955a7009cee27d371f8031c72b2b618
Andrey Shchekin <ashmind@gmail.com>
2014-12-09 22:27:52 +1300

    Corrected DryIoc brace position for consistency with other adapters.

--- DependencyInjection/Adapters/DryIocAdapter.cs
+++ DependencyInjection/Adapters/DryIocAdapter.cs
@@ -6,61 +6,50 @@ using FeatureTests.On.DependencyInjection.Adapters.Interface;
 
 namespace FeatureTests.On.DependencyInjection.Adapters
 {
-    public class DryIocAdapter : ContainerAdapterBase
-    {
+    public class DryIocAdapter : ContainerAdapterBase {
         private readonly Container container = new Container(
-            rules => rules.With(Constructor.WithAllResolvableArguments,
-                propertiesAndFields: PropertiesAndFields.PublicNonPrimitive));
+            rules => rules.With(Constructor.WithAllResolvableArguments, propertiesAndFields: PropertiesAndFields.PublicNonPrimitive)
+        );
 
         private Container webRequestScoped;
 
-        public override Assembly Assembly
-        {
+        public override Assembly Assembly {
             get { return typeof(Container).Assembly; }
         }
 
-        public override string PackageId
-        {
+        public override string PackageId {
             get { return "DryIoc.dll"; }
         }
 
-        public override void RegisterSingleton(Type serviceType, Type implementationType)
-        {
+        public override void RegisterSingleton(Type serviceType, Type implementationType) {
             container.Register(serviceType, implementationType, Reuse.Singleton);
         }
 
-        public override void RegisterTransient(Type serviceType, Type implementationType)
-        {
+        public override void RegisterTransient(Type serviceType, Type implementationType) {
             container.Register(serviceType, implementationType);
         }
 
-        public override void RegisterPerWebRequest(Type serviceType, Type implementationType)
-        {
+        public override void RegisterPerWebRequest(Type serviceType, Type implementationType) {
             container.Register(serviceType, implementationType, Reuse.InCurrentScope);
         }
 
-        public override void RegisterInstance(Type serviceType, object instance)
-        {
+        public override void RegisterInstance(Type serviceType, object instance) {
             container.RegisterInstance(serviceType, instance);
         }
 
-        public override void AfterBeginWebRequest()
-        {
+        public override void AfterBeginWebRequest() {
             this.webRequestScoped = container.BeginScope();
         }
 
-        public override void BeforeEndWebRequest()
-        {
+        public override void BeforeEndWebRequest() {
             this.webRequestScoped.Dispose();
         }
 
-        public override object Resolve(Type serviceType)
-        {
+        public override object Resolve(Type serviceType) {
             return container.Resolve(serviceType);
         }
 
-        public override IEnumerable<object> ResolveAll(Type serviceType)
-        {
+        public override IEnumerable<object> ResolveAll(Type serviceType) {
             return container.ResolveMany<object>(serviceType);
         }
     }
